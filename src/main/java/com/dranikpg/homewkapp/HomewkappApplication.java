0ed1081d4bd34e96b593175695ce960c73ce1da0
DranikProgrammer <vladislav.oleshko@gmail.com>
2018-06-20 18:59:54 +0300

    Initial commit

---
+++ src/main/java/com/dranikpg/homewkapp/HomewkappApplication.java
@@ -0,0 +1,12 @@
+package com.dranikpg.homewkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomewkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomewkappApplication.class, args);
+	}
+}
